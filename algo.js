8f8d7be04a39692d287d39204bc74ba924802b69
ibtihelgombra <betigomgom@gmail.com>
2024-03-20 00:01:40 +0000

    Checkpoint Sorting and searching algorithms

---
+++ algo.js
@@ -0,0 +1,20 @@
+function insertionSort(arr) {
+    const n = arr.length;
+    for (let i = 1; i < n; i++) {
+        let current = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+        console.log(`Array after iteration ${i}:`, arr.slice(0, i + 1)); 
+    }
+    return arr;
+}
+
+
+const array = [12, 11, 13, 5, 6];
+console.log("Original array:", array);
+const sortedArray = insertionSort(array);
+console.log("Sorted array:", sortedArray);
